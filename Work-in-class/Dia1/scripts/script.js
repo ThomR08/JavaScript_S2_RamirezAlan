540d71b98297ad44bd0ce2421dc8b5a57cf23761
Alan Ramirez (Cl Cajasan) <thomasramirez0806@gmail.com>
2025-07-18 13:07:24 -0500

    feat :sparkles: Add work in class

---
+++ Work-in-class/Dia1/scripts/script.js
@@ -0,0 +1,46 @@
+// #############################
+// ########## Día 1 ############
+// #############################
+
+// ¡¡¡Primer comando!!!
+
+console.log("Hola Mundo");
+variable1 = 3.14;
+console.log(variable1);
+console.log(typeof (variable1));
+console.log(typeof variable1, variable1);
+
+palabra1="Campus";
+palabra2="Lands";
+palabra3=palabra1+palabra2;
+console.log(palabra3);
+
+// Boleano
+
+boleanito1=true;
+boleanito2=false;
+console.log(typeof boleanito1)
+
+// Condicionales
+
+if(boleanito1 == true){
+    console.log("Es verdadero!!");
+}else{
+    console.log("No es verdadero :saidfeis:");
+}
+
+// Bucles
+
+for(i=0; i<4; i++){
+    console.log("Wenas", i);
+}
+
+holi = true;
+while(holi==true){
+    console.log("Holis");
+    holi=false;
+}
+
+
+
+// Desarrollado por: Alan Ramírez - T.I. 1096702159
