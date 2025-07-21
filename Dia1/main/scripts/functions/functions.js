a684b1a97c789fd6d608814c5dda8d971fb79d35
Alan Ramirez (Cl Cajasan) <thomasramirez0806@gmail.com>
2025-07-21 08:34:27 -0500

    feat :sparkles: Developing code

---
+++ Dia1/main/scripts/functions/functions.js
@@ -0,0 +1,14 @@
+function mainMenu() {
+    console.log("""=============================================
+    Simulador de Gasto Diario
+=============================================
+Seleccione una opción:
+
+1. Modificar gastos
+2. Listar gastos
+3. Calcular total de gastos
+4. Generar reporte de gastos
+5. Salir
+=============================================
+""")
+}
---
+++ Dia1/main/scripts/script.js
@@ -0,0 +1,18 @@
+// Proyecto Python a JavaScript
+// Link proyecto Python: https://github.com/ThomR08/Proyecto_Python_RamirezAlan
+
+// Simulador de Gasto Diario
+
+// Importar funciones
+
+// Iniciar programa
+console.clear()
+console.log("Bienvenido al seguimiento de gastos\n")
+
+// While para mantener el menu activo
+keepMainMenu = false
+while(keepMainMenu){
+}
+
+
+// Developed by: Alan Ramirez - T.I 1096702159
