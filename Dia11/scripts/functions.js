064cb4effd00ee8aec06ee06f89ca2742d14f0a4
Alan Thomas Ramirez Vargas <thomasramirez0806@gmail.com>
2025-08-01 21:25:06 -0500

    feat :sparkles: Add draw a card

--- Dia11/scripts/functions.js
+++ Dia11/scripts/functions.js
@@ -13,3 +13,26 @@ export async function startGame() {
         xhr.send();
     })
 }
+
+export async function drawCard(deckId) {
+    return new Promise((resolve) => {
+        const xhr = new XMLHttpRequest();
+        const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
+        xhr.open("GET", url, true);
+        xhr.onload = function () {
+            if (xhr.status == 200) {
+                const response = JSON.parse(xhr.responseText);
+                const code = response["cards"][0]["code"];
+                const image = response["cards"][0]["image"];
+                const remaining = response["remaining"]
+                const cardJSON = {
+                    "code": code,
+                    "image": image,
+                    "remaining": remaining
+                };
+                resolve(cardJSON);
+            }
+        }
+        xhr.send();
+    })
+}
--- Dia11/scripts/script.js
+++ Dia11/scripts/script.js
@@ -2,3 +2,13 @@ import * as name from "./functions.js";
 
 const deckId = await name.startGame();
 console.log(deckId);
+
+const card1 = await name.drawCard(deckId)
+console.log(card1["code"])
+console.log(card1["image"])
+console.log(card1["remaining"]);
+
+const card2 = await name.drawCard(deckId)
+console.log(card2["code"])
+console.log(card2["image"])
+console.log(card2["remaining"]);
